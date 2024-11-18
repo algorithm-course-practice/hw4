4a0e303a164f7e6e34b2e3c6a7aece92c31595fe
vitaly <vitaly.e.belkin@gmail.com>
2024-11-19 02:35:28 +0300

    Vitaliy Belkin HW4

--- src/main/java/org/example/TicketManagerImpl.java
+++ src/main/java/org/example/TicketManagerImpl.java
@@ -1,20 +1,41 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class TicketManagerImpl implements TicketManager{
 
-    private final Ticket[] tickets;
+    /**
+     * Очередь для талонов с типом "pension"
+     */
+    private final List<Ticket> pensionQueue = new LinkedList<>();
 
-    public TicketManagerImpl(Ticket[] tickets) {
-        this.tickets = tickets;
-    }
+    /**
+     * Очередь для талонов с другими типами
+     */
+    private final List<Ticket> regularQueue = new LinkedList<>();
 
     @Override
     public void add(Ticket ticket) {
-
+        if ("pension".equals(ticket.type)) {
+            pensionQueue.add(ticket);
+        } else {
+            regularQueue.add(ticket);
+        }
     }
 
     @Override
     public Ticket next() {
+
+        // Проверяем приоритетную очередь
+        if (!pensionQueue.isEmpty()) {
+            return pensionQueue.remove(0); // Удаляем и возвращаем первый элемент
+        }
+        // Проверяем обычную очередь
+        if (!regularQueue.isEmpty()) {
+            return regularQueue.remove(0); // Удаляем и возвращаем первый элемент
+        }
+        // Если все очереди пусты
         return null;
     }
 }
